b0f414568e860b7a6fb1e49ae17b95ff4b67713d
LuYixian <52877870+LuYixian@users.noreply.github.com>
2022-06-21 15:55:00 +0900

    Initialize tests

---
+++ tests/test_utils.py
@@ -0,0 +1,17 @@
+import pytest
+
+
+def test_start(tmp_path):
+    pass
+
+
+def test_finish(tmp_path):
+    pass
+
+
+def test_instantiate_callbacks(tmp_path):
+    pass
+
+
+def test_instantiate_loggers(tmp_path):
+    pass
